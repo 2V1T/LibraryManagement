bcbfc88e72fdd28339b8a9f822799e8d952444a2
DKVI <ct06n0164@actvn.edu.vn>
2024-09-01 08:09:37 +0700

    add form info, form login

--- LibraryManagement/obj/Debug/net8.0-windows/LibraryManagement.AssemblyInfo.cs
+++ LibraryManagement/obj/Debug/net8.0-windows/LibraryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46bc09bd30bec80a992d96aa160f497cc41b12e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed161f35f25c1e79a501b46d415de46de62e197e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
